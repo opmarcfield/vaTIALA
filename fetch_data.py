08684449728e6506255931863fab24c9d9499e7e
opmarcfield <39411656+opmarcfield@users.noreply.github.com>
2025-11-06 09:27:40 +0200

    Update fetch_data.py
    add random ass shellbane gryphon

--- fetch_data.py
+++ fetch_data.py
@@ -26,7 +26,7 @@ MINIGAMES = [
     "Grotesque Guardians", "Hespori", "Kalphite Queen", "King Black Dragon", 
     "Kraken", "Kree'Arra", "K'ril Tsutsaroth", "Lunar Chests", "Mimic", "Nex", 
     "Nightmare", "Phosani's Nightmare", "Obor", "Phantom Muspah", "Sarachnis", 
-    "Scorpia", "Scurrius", "Skotizo", "Sol Heredit", "Spindel", "Tempoross", 
+    "Scorpia", "Scurrius", "Shellbane Gryphon", "Skotizo", "Sol Heredit", "Spindel", "Tempoross", 
     "The Gauntlet", "The Corrupted Gauntlet", "The Hueycoatl", "The Leviathan", "The Royal Titans", 
     "The Whisperer", "Theatre of Blood", 
     "Theatre of Blood: Hard Mode", "Thermonuclear Smoke Devil", "Tombs of Amascut", "Tombs of Amascut - Expert Mode", "TzKal-Zuk", "TzTok-Jad", 
